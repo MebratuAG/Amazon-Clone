257f2732f32246dfdeb1e95abcd47397e90cc9a2
MebratuAG <mebratuantoniyos@gmail.com>
2024-11-27 17:24:54 +0100

    Category Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Components/Header/Header.jsx";
 import "./App.css";
 import Carousel from "./Components/Carousel/Carousel.jsx";
+import Category from "./Components/Category/Category.jsx";
 
 function App() {
   return (
     <div>
       <Header />
       <Carousel />
+      <Category />
     </div>
   );
 }
