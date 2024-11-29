6613fa8447bd293244436ea56c854caa93faba56
MebratuAG <mebratuantoniyos@gmail.com>
2024-11-29 18:35:14 +0100

    CategoryRouting Added

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Results.module.css";
 import LayOut from "../../Components/LayOut/LayOut";
+import { useParams } from "react-router-dom";
+import ProductCard from "../../Components/Product/ProductCard.jsx";
+import { productUrl } from "../../Api/endPoints.js";
+import axios from "axios";
 
 function Results() {
+  const [results, setResults] = useState([]);
+  const { categoryName } = useParams();
+  useEffect(() => {
+    axios
+      .get(`${productUrl}/products/category/${categoryName}`)
+      .then((res) => {
+        setResults(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
   return (
     <LayOut>
-      <div>Rusults</div>
+      <section>
+        <h1 style={{ padding: "30px" }}>Results</h1>
+        <p style={{ padding: "30px" }}>Category / {categoryName}</p> <hr />
+        <div className={classes.products_container}>
+          {results?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
     </LayOut>
   );
 }
-
 export default Results;
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landing from './Pages/Landing/Landing';
+import Landing from "./Pages/Landing/Landing";
 import SignIn from "./Pages/Auth/SignIn";
-import Payment from './Pages/Payment/Payment';
-import Orders from './Pages/Orders/Orders';
-import Cart from './Pages/Cart/Cart';
+import Payment from "./Pages/Payment/Payment";
+import Orders from "./Pages/Orders/Orders";
+import Cart from "./Pages/Cart/Cart";
+import Results from "./Pages/Results/Results.jsx";
 function Routing() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/auto" element={<SignIn/>} />
+        <Route path="/auto" element={<SignIn />} />
         <Route path="/payments" element={<Payment />} />
-        <Route path="/orders" element={<Orders/>} />
-        <Route path="/cart" element={<Cart/>} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/category/:categoryName" element={<Results />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );
