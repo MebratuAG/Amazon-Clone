92c614fd5400bf0839b7286252abcbf25815a6a6
MebratuAG <mebratuantoniyos@gmail.com>
2024-11-29 20:42:10 +0100

    Detail page Routing

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./ProductDetail.module.css";
 import LayOut from "../../Components/LayOut/LayOut";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+import { productUrl } from "../../Api/endPoints";
+import ProductCard from "../../Components/Product/ProductCard";
+// import Loader from "../../Components/Loader/Loader.jsx";
 
 function ProductDetail() {
+  const { productId } = useParams();
+  const [product, setproduct] = useState({});
+  useEffect(() => {
+    axios
+      .get(`${productUrl}/products/${productId}`)
+      .then((res) => {
+        setproduct(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <LayOut>
-      <div>ProductDetail</div>
+      <ProductCard product={product} />
     </LayOut>
   );
 }
